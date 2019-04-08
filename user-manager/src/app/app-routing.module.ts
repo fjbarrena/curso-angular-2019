7d9bfcfc8461a6abb51212afaaed93b51e2d7955
Martin <martin.zaneda@gmail.com>
2019-04-08 16:22:04 +0200

    Lazy loading base

---
+++ user-manager/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './modules/dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: 'users',
+    loadChildren: './modules/users/users.module#UsersModule'
+  },
+  {
+    path: 'login',
+    loadChildren: './modules/login/login.module#LoginModule'
+  },
+  {  path: '**', redirectTo: 'login' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
